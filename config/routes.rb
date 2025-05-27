0f57994009cb9e2f9e1fe9a5d7592a3c72baf3ed
IvanPagani <ivanpagani07@gmail.com>
2025-05-27 08:47:13 -0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "articles#index"
+  
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+  
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+end
